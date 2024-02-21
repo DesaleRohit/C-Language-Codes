86c2608e9ee549998741b6034748947a56172d8b
DesaleRohit <rohitdesale2005@gmail.com>
2024-02-21 22:35:41 +0530

    [ADDED]:Rohit -C Program to Calculate Total Amount of Ice Cream with Discount

---
+++ Add_sub_div_mul.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int a , b, Addition=0, Subtraction=0, Division=0, Multiplication=1;
+	
+	printf("\nEnter the first number:");
+	scanf("%d",&a);
+	
+	printf("\nEnter the second number:");
+	scanf("%d", &b);
+	
+	Addition=a+b;
+	Subtraction=a-b;
+	Division=a/b;
+	Multiplication=a*b;
+	
+	printf("\nAddition:%d",Addition);
+	printf( "\nSubtraction:%d", Subtraction);
+	printf( "\nDivision:%d", Division);
+	printf( "\nMultiplication:%d", Multiplication);
+		
+	return 0;
+}
