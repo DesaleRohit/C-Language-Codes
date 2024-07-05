2f3d763ef3f76a4488dbd968aaed7a58b0f8759f
DesaleRohit <rohitdesale2005@gmail.com>
2024-07-05 10:50:01 +0530

    [ADDED]:Rohit -Nested-if-else code

---
+++ Nested-if-else.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+	int A,B,C;
+	printf("\nEnter the value of A:");
+	scanf("%d",&A);
+	printf("\nEnter the value of B:");
+	scanf("%d",&B);
+	printf("\nEnter the value of c:");
+	scanf("%d",&C);
+	if( A > B)
+	{
+		if( A > C)
+		{
+			printf("\nA is Big");
+		}
+		else
+		{
+			printf("\nC is Big");
+		}
+	}
+	else
+	{
+		if( B > C)
+		{
+			printf("\n B is Big");
+		}
+		else
+		{
+			printf("\n C is big");
+		}
+	}
+	return 0;
+}
